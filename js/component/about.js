95f8acd3a065c15e5c7367ed0b9306c1c83f9053
Xin Tong <xtong@thoughtworks.com>
2016-09-04 19:13:18 +0800

    1

--- js/component/about.js
+++ js/component/about.js
@@ -10,7 +10,7 @@ class About extends Component {
 
         return (
             <div>
-                This is for react and react-router practice
+                This is for react and react-router practice 1
             </div>
         )
     };
